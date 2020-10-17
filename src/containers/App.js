ac3c51f2927303583f072175deb97f307c33aac6
Venkatesh <venky.ch59@gmail.com>
2020-10-17 17:28:30 +0000

    initial commit

---
+++ src/containers/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { configureStore } from '../store/index';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+const store = configureStore();
+
+const App = () => (
+	<Provider store={store}>
+		<Router>
+			<div>
+			Hello World!
+			</div>
+		</Router>
+	</Provider>
+)
+
+export default App;
